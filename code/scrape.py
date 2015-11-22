d04ca80c73d0398fb7b6b9ee1bd1e952d4de656e
john-dickinson <johndi@umich.edu>
2015-11-21 21:25:39 -0500

    formatting changes

--- code/scrape.py
+++ code/scrape.py
@@ -24,7 +24,7 @@ youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVE
 def setup():
 	query = raw_input("Enter your query: ")
 	argparser.add_argument("--q", help="Search term", default=query) #change the default to the search term you want to search
-	argparser.add_argument("--max-results", help="Max results", default=1) #default number of results which are returned. It can vary from 0 - 100
+	argparser.add_argument("--max-results", help="Max results", default=50) #default number of results which are returned. It can vary from 0 - 100
 	options = argparser.parse_args()
 	search_response = youtube.search().list( # Call the search.list method to retrieve results matching the specified query term.
 	 q=options.q,
@@ -38,9 +38,9 @@ def setup():
 def scrape(search_response):
 	videos = {} # Add each result to the appropriate list, and then display the lists of matching videos. Filter out channels, and playlists.
 	for search_result in search_response.get("items", []):
-	 if search_result["id"]["kind"] == "youtube#video": #videos.append("%s" % (search_result["id"]["videoId"]))
-	    videos[search_result["id"]["videoId"]] = search_result["snippet"]["title"]
-		#print "Videos:\n", "\n".join(videos), "\n"
+		if search_result["id"]["kind"] == "youtube#video": #videos.append("%s" % (search_result["id"]["videoId"]))
+			videos[search_result["id"]["videoId"]] = search_result["snippet"]["title"]
+			#print "Videos:\n", "\n".join(videos), "\n"
 	s = ','.join(videos.keys())
 
 	videos_list_response = youtube.videos().list(
@@ -50,7 +50,6 @@ def scrape(search_response):
 	res = []
 
 	#print videos_list_response['items']
-
 	for i in videos_list_response['items']:
 		temp_res = dict(v_id = i['id'], v_title = videos[i['id']])
 		temp_res.update(i['statistics'])
@@ -60,8 +59,8 @@ def scrape(search_response):
 		dislikes = i['statistics']['dislikeCount']
 		likes = i['statistics']['likeCount']
 		reputability = int(likes) / int(dislikes)
-		if reputability >= 0.7:
-		 res.append(temp_res)
+		if reputability >= 0.90:
+			res.append(temp_res)
 
 
 	df = pd.DataFrame.from_dict(res)
